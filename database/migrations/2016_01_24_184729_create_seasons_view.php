4794029118aca5fd581f679b74b03d7c94bd03e9
Lino Steenkamp <lino@steenkamps.org>
2018-02-04 12:23:20 +0200

    Update 2016_01_24_184729_create_seasons_view.php
    Add archived to season status where clause

--- database/migrations/2016_01_24_184729_create_seasons_view.php
+++ database/migrations/2016_01_24_184729_create_seasons_view.php
@@ -12,7 +12,7 @@ class CreateSeasonsView extends Migration
      */
     public function up()
     {
-        DB::statement('create view seasons as (select cast(concat(9, season, show_id) as SIGNED) as id, show_id AS show_id, season AS season, sum(file_size) AS file_size from episodes where status = \'downloaded\' group by show_id, season)');
+        DB::statement('create view seasons as (select cast(concat(9, season, show_id) as SIGNED) as id, show_id AS show_id, season AS season, sum(file_size) AS file_size from episodes where status in \( \'downloaded\', \'archived\'\) group by show_id, season)');
     }
 
     /**
